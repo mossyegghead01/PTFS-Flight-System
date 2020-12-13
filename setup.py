0c1ca5b55901c106d92b58bb366b0395164b91d9
mossyegghead01 <69193776+mossyegghead01@users.noreply.github.com>
2020-12-13 10:18:11 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='swas.py',
+    packages=find_packages(include=['flight system']),
+    version='1.0.0',
+    description='none',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mossyegghead01/BA-Flight-System",
+    author='mossy the mad guy, DistinctNoot',
+    license='MIT',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+)
